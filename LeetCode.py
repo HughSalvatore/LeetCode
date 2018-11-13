438765ab3aaa9dbd930ba10c5b88c3fa1e689cbc
HughSalvatore <haoyu15926@foxmail.com>
2018-11-13 15:21:25 +0800

    add linkList class and run test

--- LeetCode.py
+++ LeetCode.py
@@ -43,14 +43,107 @@ def isPalindrome(target, i, j):
             return False
     return True
 
+class ListNode():
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+    def get_data(self):
+        return self.val
+
+    def set_data(self, new_data):
+        self.val = new_data
+    
+    def get_next(self):
+        return self.next
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+class SingLinkList():
+    def __init__(self):
+        self.head = None
+        self._length = 0
+
+    def getLength(self):
+        return self._length
+
+    def isEmpty(self):
+        return self.head == None
+
+    def isLast(self, node):
+        return node.get_next() == None
+
+    def append(self, dataOrNode):
+        item = None
+        if isinstance(dataOrNode, ListNode):
+            item = dataOrNode
+        else:
+            item = ListNode(dataOrNode)
+
+        if self.head == None:
+            self.head = item
+            self._length += 1
+        else:
+            ref = self.head
+            while ref.get_next():
+                ref = ref.get_next()
+            ref.set_next(item)
+            self._length += 1
+
+    def find(self, value):
+        ref = self.head
+        while ref != None and ref.get_data() != value:
+            ref = ref.get_next()
+        return ref
+
+    def findprevious(self, value):
+        ref = self.head
+        while ref.get_next() != None and ref.get_next().get_data() != value:
+            ref = ref.get_next()
+        return ref
+        
+
+    def delete(self, value):
+        toDelPreNode = self.findprevious(value)
+        if toDelPreNode.get_next() != None:
+            toDelPreNode.set_next(toDelPreNode.get_next().get_next())
+            self._length -= 1
+
+    def insert(self, value, position):
+        targetNode = ListNode(value)
+        targetNode.set_next(position.get_next())
+        position.set_next(targetNode)
+        self._length += 1
+
+    def printLinkList(self):
+        ref = self.head
+        while ref != None:
+            print(ref.get_data())
+            ref = ref.get_next()
      
-
-           
 
 if __name__ == '__main__':
     # array = [1, 4, 2, 8, 5, 7]
     # (i, j) = findTwoNumSum(array, 12)
     # print(i, j)
-    print(isSquare(11))
+    # print(isSquare(11))
     # print(isPalindrome("abba"))
+    linklist = SingLinkList()
+    for i in range(6):
+        linklist.append(i)
+    linklist.printLinkList()
+    # print("deleted 2")
+    # linklist.delete(2)
+    # linklist.printLinkList()
+    # print("delete 5")
+    # linklist.delete(5)
+    # linklist.printLinkList()
+    targetNode = linklist.find(3)
+    linklist.insert(9, targetNode)
+    linklist.printLinkList()
+    targetNode = linklist.findprevious(9)
+    linklist.insert(10, targetNode)
+    linklist.printLinkList()
+    print(linklist.getLength())
+
