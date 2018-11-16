f1e96c9795ec62cf7e7576977e6dedec9c516116
HughSalvatore <haoyu15926@foxmail.com>
2018-11-16 15:46:57 +0800

    add three types of  Traversing for BinaryTree Class

--- DynamicPro.py
+++ DynamicPro.py
@@ -42,14 +42,87 @@ class BinaryTree:
                     queue.append(popNode.left)
                 if popNode.right != None:
                     queue.append(popNode.right)
-    
 
+    # 前序遍历
+    def printPreOrder(self):
+        if self.root == None:
+            print("The binary tree is not existed")
+        else:
+            stack = list()
+            outputlist = list()
+            popNode = self.root
+            while len(stack) or popNode != None:
+                if popNode is None:
+                    popNode = stack.pop().right
+                    continue
+
+                while popNode.left != None:
+                    stack.append(popNode)
+                    outputlist.append(popNode.element)
+                    # print(popNode.element)
+                    popNode = popNode.left
+                    
+                outputlist.append(popNode.element)
+                # print(popNode.element)
+                popNode = popNode.right
+            print(outputlist)
+
+    # 中序遍历
+    def printMediumOrder(self):
+        if self.root is None:
+            print("The binary tree is not existed.")
+        else:
+            stack = list()
+            outputlist = list()
+            popNode = self.root
+            while len(stack) or popNode != None:
+                while popNode.left is not None:
+                    stack.append(popNode)
+                    popNode = popNode.left
+
+                outputlist.append(popNode.element)
+
+                popNode = stack.pop()
+                outputlist.append(popNode.element)
+
+                popNode = popNode.right
+            print(outputlist)            
+    
+    # 后续遍历
+    def printPostOrder(self):
+        if self.root == None:
+            print("The binary tree is not existed.")
+        else:
+            stack = list()
+            outputlist = list()
+            popNode = self.root
+            while len(stack) or popNode != None:
+                if popNode is None:
+                    popNode = stack.pop().left
+                    continue
+
+                while popNode.right is not None:
+                    stack.append(popNode)
+                    outputlist.append(popNode.element)
+                    popNode = popNode.right
+                outputlist.append(popNode.element)
+                popNode = popNode.left
+
+            print(outputlist[::-1])
+                
+
+                
+
+                    
+               
+                
 if __name__ == "__main__":
     arr = [1, 4, 2, 8, 5, 7]
     bTree = BinaryTree()
     for i in arr:
-        bTree.insertNodeInOrder(i)
-    
-    bTree.printBinaryTree()
-
+        bTree.insertNodeInOrder(i)  
+    # bTree.printBinaryTree()
+    # bTree.printPreOrder()  
+    # bTree.printPostOrder()
+    bTree.printMediumOrder()
     
